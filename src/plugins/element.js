93b73eab4c0eedc0f381a600a602cd740783e553
limengq <limengq@yonyou.com>
2020-12-26 14:32:33 +0800

    完成了权限功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -6,7 +6,8 @@ import Vue from 'vue'
 // import {Message} from 'element-ui'
 import {Button, Form, FormItem, Input, Message, Container, Header, Aside,Main, 
 Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table,
-TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox} from 'element-ui'
+TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree,
+Select, Option} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -30,5 +31,9 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
 Vue.prototype.$message=Message
 Vue.prototype.$confirm = MessageBox.confirm
