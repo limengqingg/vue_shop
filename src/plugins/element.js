7324d80748794502c8b6108a3d111fca9eb7c4cc
limengq <limengq@yonyou.com>
2021-01-04 10:19:27 +0800

    完成了分类参数的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -7,7 +7,7 @@ import Vue from 'vue'
 import {Button, Form, FormItem, Input, Message, Container, Header, Aside,Main, 
 Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table,
 TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree,
-Select, Option,Cascader} from 'element-ui'
+Select, Option,Cascader,Alert,Tabs,TabPane} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -36,5 +36,8 @@ Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(Tabs)
+Vue.use(TabPane)
 Vue.prototype.$message=Message
 Vue.prototype.$confirm = MessageBox.confirm
